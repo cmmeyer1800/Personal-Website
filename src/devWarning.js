4923359615f03db2b62a5db7492d56c957819447
Collin Meyer <collinmmeyer@gmail.com>
2023-09-20 17:44:03 -0500

    added warning for test as well

--- src/devWarning.js
+++ src/devWarning.js
@@ -13,6 +13,14 @@ const DevWarning = () => {
             </div>
         </div>
         }
+        { process.env.REACT_APP_ENV === "test" && shown &&
+        <div className='hero is-danger'>
+            <a href="/null" className="mt-1 ml-1" onClick={(e) => {e.preventDefault(); setShown(false)}}>Hide</a>
+            <div className='hero-body has-text-centered title'>
+            Warning: This is a test build, the production site is at<a href="https://collinmmeyer.com">: collinmmeyer.com</a>
+            </div>
+        </div>
+        }
         </>
     )
 }
