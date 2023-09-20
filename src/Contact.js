9fc51300416667d8384f1e4474f65939033d6841
Collin Meyer <collinmmeyer@gmail.com>
2023-09-19 23:32:08 -0500

    added contact options

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
 
 const Contact = () => {
     return (
-        <>
-        </>
+        <div className="columns">
+            <div className="column has-text-centered">
+                <Link to="https://www.linkedin.com/in/collin-meyer-88593a206/">
+                    <img src="/linkedin.webp">
+                    </img>
+                </Link>
+            </div>
+            <div className="column has-text-centered">
+                <Link to="mailto:collinmmeyer@gmail.com">
+                    <img src="/mail_logo.webp">
+                    </img>
+                </Link>
+            </div>
+            {/* <div className="column has-text-centered">
+
+            </div> */}
+        </div>
     );
 };
 
