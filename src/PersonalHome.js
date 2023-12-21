6290067effeea7b6a51a9aa9c82d17d02165327d
Collin Meyer <collinmmeyer@gmail.com>
2023-12-21 14:11:52 -0600

    switched to purely webp, more edits

--- src/PersonalHome.js
+++ src/PersonalHome.js
@@ -4,14 +4,14 @@ const Photo = (props) => {
     const [expanded, setExpanded] = useState(false);
 
     return (
-        <div class={`column ${props.size_args}`}>
-            <div class="card">
-                <div class="card-image">
-                    <figure class="image is-3by2">
+        <div className={`column ${props.size_args}`}>
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-3by2">
                         <img src={props.url} alt={props.alt}></img>
                     </figure>
-                    <div class="card-content is-overlay is-clipped">
-                        <a class="tag is-dark" href='#none' onClick={(e) => {
+                    <div className="card-content is-overlay is-clipped">
+                        <a className="tag is-dark" href='#none' onClick={(e) => {
                             e.preventDefault();
                             setExpanded(!expanded)
                         }}>
@@ -29,11 +29,11 @@ const Photo = (props) => {
 
 const Gallery = () => {
     return (
-        <div className="ml-2 mr-2">
-            <div class="columns is-multiline">
+        <div className="ml-3 mr-3">
+            <div className="columns is-multiline">
                 <Photo url="/michigan-whole-team.webp" alt="Winning SAE Michigan 2023"
-                       size_args="is-one-third-desktop is-half-tablet"/>
-                <Photo url="/michigan-whole-team.webp" alt="Winning SAE Michigan 2023"
+                       size_args=""/>
+                <Photo url="/fsae-testing-overhead.webp" alt="Testing our FSAE car, shot from drone"
                        size_args="is-one-third-desktop is-half-tablet"/>
             </div>
         </div>
